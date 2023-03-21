fae0a90e39b6e858d7d148f74511f117c0777855
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-03-21 13:49:27 -0700

    conditional function to display remaining days

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,6 +6,16 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
 
+  function formatSpots() {
+    if (props.spots === 1) {
+      return '1 spot remaining'
+    }
+    if (props.spots === 0) {
+      return 'no spots remaining'
+    }
+    return `${props.spots} spots remaining`
+  }
+
   const dayClass = classNames(
     "day-list__item", {
       "day-list__item--selected": props.selected,
@@ -18,7 +28,7 @@ export default function DayListItem(props) {
     className={dayClass} 
     onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
