0729761c99783bf2bb3d347896fef3ac43097d16
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-03-22 15:32:32 -0700

    add appointment components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "components/Appointment/styles.scss";
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment"></article>
+  );
+}
