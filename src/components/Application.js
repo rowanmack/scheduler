819df37363212f2286608e8294137d05f7360508
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-03-23 20:15:56 -0700

    adding promises result to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -53,7 +53,8 @@ export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -66,8 +67,8 @@ export default function Application() {
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers')
-    ]).then((response) => {
-      setState(prev => ({...prev, days: response[0].data, appointments: response[1].data, interviewers:response[2].data }))
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers:all[2].data }))
     });
   }, []);
 
